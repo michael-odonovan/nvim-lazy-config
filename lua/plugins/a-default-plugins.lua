8d9d6b65059d4f604546afd8d8761fce8710b6e1
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-05-06 21:19:44 +0100

    colorscheme fix

--- lua/plugins/a-default-plugins.lua
+++ lua/plugins/a-default-plugins.lua
@@ -1,5 +1,13 @@
 return {
 
+  -- colorscheme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function() vim.cmd.colorscheme 'catppuccin' end,
+  },
+
   { 'tpope/vim-fugitive' },
 
   { 'kdheepak/lazygit.nvim' },
