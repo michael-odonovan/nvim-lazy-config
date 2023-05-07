1d73968422276d616b0a44d1bdc45abe22498c8e
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-05-07 17:37:43 +0100

    update

--- init.lua
+++ init.lua
@@ -1,19 +1,21 @@
 
--- lazy setup
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- lazy package manager
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath
-  })
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
--- mapleader etc.
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.opt.termguicolors = true
 
 -- note plugins cannot be renamed!!!
 require('lazy').setup('plugins')
