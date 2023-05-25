5c0682e174f8f870b89081d1e33e509f5e95a3e5
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-05-25 16:47:54 +0100

    update

--- lua/user/user-commands.lua
+++ lua/user/user-commands.lua
@@ -5,5 +5,6 @@ local M = {}
 -- vim.cmd.Test()
 
 vim.api.nvim_create_user_command('Tickets', 'edit ~/repos/tickets/', {})
+vim.api.nvim_create_user_command('Errors', 'edit ~/Desktop/errors.md', {})
 
 return M
