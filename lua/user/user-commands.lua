5e486f0abb5c3c3109b63696e8475762ae871f21
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-05-28 17:15:53 +0100

    update

--- lua/user/user-commands.lua
+++ lua/user/user-commands.lua
@@ -6,5 +6,6 @@ local M = {}
 
 vim.api.nvim_create_user_command('Tickets', 'edit ~/repos/tickets/', {})
 vim.api.nvim_create_user_command('Errors', 'edit ~/Desktop/errors.md', {})
+vim.api.nvim_create_user_command('Tmux', 'edit ~/.tmux.conf', {})
 
 return M
