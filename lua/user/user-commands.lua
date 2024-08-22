03ce52b1739c53f277d511ef251b79e93c9b7640
Micheal O'Donovan <mikeodonovan3@gmail.com>
2024-08-22 10:30:42 +0100

    update

--- lua/user/user-commands.lua
+++ lua/user/user-commands.lua
@@ -23,9 +23,9 @@ vim.api.nvim_create_user_command('Pwd', "put =expand('%:p')", {})
 
 vim.api.nvim_create_user_command('Desk', 'edit ~/Desktop/', {})
 
-vim.api.nvim_create_user_command('Todo', 'edit ~/repos/planning/1.today-todo.md', {})
+vim.api.nvim_create_user_command('Todo', 'edit ~/repos/notes/planning/1.today-todo.md', {})
 
-vim.api.nvim_create_user_command('Dig', 'edit ~/repos/planning/3.digital-standup.md', {})
+vim.api.nvim_create_user_command('Dig', 'edit ~/repos/notes/planning/3.digital-standup.md', {})
 
 vim.api.nvim_create_user_command('Storage', 'edit ~/repos/notes/storage/', {})
 
